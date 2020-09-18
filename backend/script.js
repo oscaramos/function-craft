0087697e550d6d4dfc4c1232901252b864d5da26
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-18 18:27:50 -0500

    Remove unnecesary files

--- backend/script.js
+++
@@ -1,9 +0,0 @@
-const { createFunctionFromUrl, loadMinecraftFunctionToWorldDatapack } = require('./functions');
-
-(async () => {
-  const url = 'https://www.grabcraft.com/minecraft/quartz-house-8';
-  const worldName = 'New World (6)';
-
-  const minecraftFunction = await createFunctionFromUrl(url);
-  await loadMinecraftFunctionToWorldDatapack(worldName, minecraftFunction);
-})();
