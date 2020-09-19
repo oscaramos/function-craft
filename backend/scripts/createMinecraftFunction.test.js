7bbd362324cf79e492accdb9f13adc21f6257de6
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-18 19:56:19 -0500

    Check test on createMinecraftFunction.js with snapshots

--- backend/scripts/createMinecraftFunction.test.js
+++ backend/scripts/createMinecraftFunction.test.js
@@ -1,14 +1,12 @@
 const createMinecraftFunction = require('./createMinecraftFunction')
-const fs = require('fs')
+const path = require('path')
 
-describe('create a minecraft function from javascript file, from that website, to a txt file', () => {
+describe('create a minecraft function from render object file, and check it with a snapshot', () => {
   it('Object 388', () => {
-    const minecraftFunction = createMinecraftFunction('./datatest/myRenderObject_388.js', )
-    fs.writeFileSync('outputFunction.txt', minecraftFunction)
+    expect(createMinecraftFunction(path.join('.', 'datatest', 'myRenderObject_388.js'))).toMatchSnapshot()
   })
 
   it('Object 397 from https://www.grabcraft.com/minecraft/quartz-house-8', () => {
-    const minecraftFunction = createMinecraftFunction('./datatest/myRenderObject_397.js', 'outputFunction.txt')
-    fs.writeFileSync('outputFunction.txt', minecraftFunction)
+    expect(createMinecraftFunction(path.join('.', 'datatest', 'myRenderObject_397.js'))).toMatchSnapshot()
   })
 });
