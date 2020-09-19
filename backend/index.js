34d92a0c32eabba7406c9436285ee669547ced70
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-18 20:23:27 -0500

    breaking change!: remove createFunctionFromUrl()
    Instead use createMinecraftHouse()

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
-const createFunctionFromUrl = require('./src/createFunctionFromUrl')
 const createMinecraftFunction = require('./src/createMinecraftFunction')
 const loadMinecraftFunctionToWorldDatapack = require('./src/loadMinecraftFunctionToWorldDatapack')
 
 module.exports = {
-  createFunctionFromUrl,
   createMinecraftFunction,
   loadMinecraftFunctionToWorldDatapack,
 }
