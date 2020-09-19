18d36773fa678e8bbb849d9706b7ae650aa2cec8
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-18 19:55:42 -0500

    Remove last useless dep from utils

--- backend/scripts/utils.js
+++ backend/scripts/utils.js
@@ -1,7 +1,5 @@
-const downloadHouse = require('./createFunctionFromUrl/downloadHouse')
 const path = require('path')
 
 module.exports = {
-  downloadHouse,
   getDirectoryHouse: url => path.join('.', 'houses', url.slice(url.lastIndexOf('/') + 1))
 }
