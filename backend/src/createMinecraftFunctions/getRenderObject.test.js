b0585d0abf21e553afe8c64868c1a4f032c5b3c7
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-23 12:52:17 -0500

    fix: file not found

--- backend/src/createMinecraftFunctions/getRenderObject.test.js
+++ backend/src/createMinecraftFunctions/getRenderObject.test.js
@@ -1,7 +1,7 @@
-const { getRenderObject } = require('../utils')
 const path = require('path')
+const getRenderObject = require('./getRenderObject')
 
 it('get javascript object from old javascript file format', () => {
-  const content = getRenderObject(path.join('.', 'datatest', 'myRenderObject_388.js'));
+  const content = getRenderObject(path.join('.', 'src', 'datatest', 'myRenderObject_388.js'));
   expect(content).toBeDefined()
 });
