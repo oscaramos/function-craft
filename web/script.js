c0b02ca0b63ba654defa6af8a25a631264104b76
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-24 12:33:42 -0500

    feat(front-end): Upgrade front-end
    Check valid URL, refactoring, shows an arrow to display downloaded file.

--- web/script.js
+++ web/script.js
@@ -1,36 +1,36 @@
-const submit = document.getElementById('submit')
-const url = document.getElementById('url')
-const mcVersion = document.getElementById('mcversion')
+const submitButton = document.getElementById('submit')
+const urlInput = document.getElementById('url')
+const mcVersionSelect = document.getElementById('mcversion')
 const downloadLink = document.getElementById('download_link')
 const loader = document.getElementById('loader')
 
 const mcVersions = [
-  "1.16.2",
-  "1.16.1",
-  "1.16",
-  "1.15.2",
-  "1.15.1",
-  "1.15",
-  "1.14.4",
-  "1.14.3",
-  "1.14.1",
-  "1.14",
-  "1.13.2",
-  "1.13.1",
-  "1.13",
-  "1.12.2",
-  "1.12.1",
-  "1.12",
-  "1.11.2",
-  "1.11",
-  "1.10.2",
-  "1.10.1",
-  "1.10",
-  "1.9.4",
-  "1.9.2",
-  "1.9",
-  "1.8.8",
-  "1.7.10",
+  '1.16.2',
+  '1.16.1',
+  '1.16',
+  '1.15.2',
+  '1.15.1',
+  '1.15',
+  '1.14.4',
+  '1.14.3',
+  '1.14.1',
+  '1.14',
+  '1.13.2',
+  '1.13.1',
+  '1.13',
+  '1.12.2',
+  '1.12.1',
+  '1.12',
+  '1.11.2',
+  '1.11',
+  '1.10.2',
+  '1.10.1',
+  '1.10',
+  '1.9.4',
+  '1.9.2',
+  '1.9',
+  '1.8.8',
+  '1.7.10',
 ]
 
 const downloadTxt = (text) => {
@@ -39,25 +39,52 @@ const downloadTxt = (text) => {
   downloadLink.click()
 }
 
-submit.addEventListener('click', async () => {
+function isValidHttpUrl(string) {
+  let url
+
+  try {
+    url = new URL(string)
+  } catch (_) {
+    return false
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:'
+}
+
+submitButton.addEventListener('click', async () => {
   // Replaces submit button with loader
-  submit.hidden = true
+  submitButton.hidden = true
   loader.hidden = false
 
+  const url = urlInput.value
+  const mc_version = mcVersionSelect.value
+
   const apiUrl = 'https://function-craft.herokuapp.com/?' + new URLSearchParams({
-    url: url.value,
-    mc_version: mcVersion.value
+    url,
+    mc_version,
   })
+
   try {
-    const data = await fetch(apiUrl)
-    const minecraftFunction = await data.text()
-    downloadTxt(minecraftFunction)
+    if (!isValidHttpUrl(url)) {
+      // change url input to error state
+      urlInput.classList.add('invalid-url')
+      throw new Error('Invalid Url')
+    } else {
+      // Downloads the minecraft function as .txt file
+      const data = await fetch(apiUrl)
+      const minecraftFunction = await data.text()
+      downloadTxt(minecraftFunction)
+      // Show an arrow to indicate where to download the file
+      Arrow.show(5000);
+      // Remove error state if it exists
+      urlInput.classList.remove('invalid-url')
+    }
   } catch (error) {
     alert(error)
   } finally {
     // Replaces loader with submit button
     loader.hidden = true
-    submit.hidden = false
+    submitButton.hidden = false
   }
 })
 
@@ -67,5 +94,6 @@ mcVersions.map(version => {
   option.setAttribute('value', version)
   option.appendChild(document.createTextNode(version))
 
-  mcVersion.appendChild(option)
+  mcVersionSelect.appendChild(option)
 })
+
